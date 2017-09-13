226cb4f953f2a5b3343641a5d2a5044adcdb6174
祝涛 <zhutao@ykse.com.cn>
2017-09-13 14:16:00 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 /**
  * Created by zhutao on 17-9-13.
  */
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import todoApp from './reducers'
+
+let store = createStore(todoApp)
+
+let rootElement = document.getElementById('root')
+render(
+<Provider store={store}>
+    <App />
+    </Provider>,
+    rootElement
+)
